00a296e47a985cae9a3f3526612db4e58e480bc6
pelhans <me@pelhans.com>
2021-03-13 22:00:29 +0800

    add crf and a little frame

---
+++ framework/sylar/log.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: log.h
+	> Author: Pelhans
+	> Mail: me@pelhans.com
+	> Created Time: Sat 13 Mar 2021 09:59:43 PM CST
+ ************************************************************************/
+
+#ifndef _LOG_H
+#define _LOG_H
+
+
+#endif
